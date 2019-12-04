7062c95e159ec933e44519aa2b667cc77799dc81
Pawel <pawel.tobys@gmail.com>
2019-12-04 14:37:03 +0100

    feat:createProject

---
+++ Modules/Comment/Providers/CommentServiceProvider.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Modules\Comment\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Factory;
+
+class CommentServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerTranslations();
+        $this->registerConfig();
+        $this->registerViews();
+        $this->registerFactories();
+        $this->loadMigrationsFrom(module_path('Comment', 'Database/Migrations'));
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(RouteServiceProvider::class);
+    }
+
+    /**
+     * Register config.
+     *
+     * @return void
+     */
+    protected function registerConfig()
+    {
+        $this->publishes([
+            module_path('Comment', 'Config/config.php') => config_path('comment.php'),
+        ], 'config');
+        $this->mergeConfigFrom(
+            module_path('Comment', 'Config/config.php'), 'comment'
+        );
+    }
+
+    /**
+     * Register views.
+     *
+     * @return void
+     */
+    public function registerViews()
+    {
+        $viewPath = resource_path('views/modules/comment');
+
+        $sourcePath = module_path('Comment', 'Resources/views');
+
+        $this->publishes([
+            $sourcePath => $viewPath
+        ],'views');
+
+        $this->loadViewsFrom(array_merge(array_map(function ($path) {
+            return $path . '/modules/comment';
+        }, \Config::get('view.paths')), [$sourcePath]), 'comment');
+    }
+
+    /**
+     * Register translations.
+     *
+     * @return void
+     */
+    public function registerTranslations()
+    {
+        $langPath = resource_path('lang/modules/comment');
+
+        if (is_dir($langPath)) {
+            $this->loadTranslationsFrom($langPath, 'comment');
+        } else {
+            $this->loadTranslationsFrom(module_path('Comment', 'Resources/lang'), 'comment');
+        }
+    }
+
+    /**
+     * Register an additional directory of factories.
+     *
+     * @return void
+     */
+    public function registerFactories()
+    {
+        if (! app()->environment('production') && $this->app->runningInConsole()) {
+            app(Factory::class)->load(module_path('Comment', 'Database/factories'));
+        }
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
